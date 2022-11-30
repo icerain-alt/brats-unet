c9b09be6c6c86aa3b2bbe21505d7b28ded60d447
Fish <72488516+icerain-alt@users.noreply.github.com>
2022-11-30 13:10:12 +0800

    Update read_h5.py

--- read_h5.py
+++ read_h5.py
@@ -1,12 +1,7 @@
 import h5py
 import numpy as np
 
-
-<<<<<<< HEAD
-p = '/data/omnisky/postgraduate/Yb/data_set/BraTS2021/all/BraTS2021_00000_mri_norm2.h5'
-=======
-p = '/***/BraTS2021/dataset/BraTS2021_00000_mri_norm2.h5'
->>>>>>> 465a5c0d4bc21b38d6085bff23b53bda8dcf9a9a
+p = '/data/omnisky/postgraduate/Yb/data_set/BraTS2021/dataset/BraTS2021_00000_mri_norm2.h5'
 h5f = h5py.File(p, 'r')
 image = h5f['image'][:]
 label = h5f['label'][:]
