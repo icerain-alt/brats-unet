344ee67172e553cbb2a68cf3d6c52faded8fc21c
Fish <72488516+icerain-alt@users.noreply.github.com>
2022-12-23 12:20:46 +0800

    Update split_data.py
    fixed error

--- data_process/split_data.py
+++ data_process/split_data.py
@@ -2,11 +2,7 @@ import os
 from sklearn.model_selection import train_test_split
 
 # 预处理输出地址
-<<<<<<< HEAD
 data_path = "/data/omnisky/postgraduate/Yb/data_set/BraTS2021/dataset"
-=======
-data_path = "/***/data_set/BraTS2021/dataset"
->>>>>>> 465a5c0d4bc21b38d6085bff23b53bda8dcf9a9a
 train_and_test_ids = os.listdir(data_path)
 
 train_ids, val_test_ids = train_test_split(train_and_test_ids, test_size=0.2,random_state=21)
@@ -16,7 +12,7 @@ print("Using {} images for training, {} images for validation, {} images for tes
 train_ids.sort()
 val_ids.sort()
 test_ids.sort()
-<<<<<<< HEAD
+
 with open('/data/omnisky/postgraduate/Yb/data_set/BraTS2021/train.txt','w') as f:
     f.write('\n'.join(train_ids))
 
@@ -24,13 +20,4 @@ with open('/data/omnisky/postgraduate/Yb/data_set/BraTS2021/valid.txt','w') as f
     f.write('\n'.join(val_ids))
 
 with open('/data/omnisky/postgraduate/Yb/data_set/BraTS2021/test.txt','w') as f:
-=======
-with open('/***/data_set/BraTS2021/train.txt','w') as f:
-    f.write('\n'.join(train_ids))
-
-with open('/***/data_set/BraTS2021/valid.txt','w') as f:
-    f.write('\n'.join(val_ids))
-
-with open('/***/data_set/BraTS2021/test.txt','w') as f:
->>>>>>> 465a5c0d4bc21b38d6085bff23b53bda8dcf9a9a
     f.write('\n'.join(test_ids))
